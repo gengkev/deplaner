c45104b342412e70aa22f69b096da39d71ae6c1d
gengkev@gmail.com <gengkev@gmail.com@199c8b54-b099-f3cb-97ca-354d9fe1cad0>
2011-02-26 23:52:29 +0000

    Some dragging stuff ready! :D

--- planeHandler.js
+++ planeHandler.js
@@ -58,13 +58,13 @@ function Plane(type,entrance,emergency) {
       otherend=[-40,randomNum(600)];
       break;
   }
-  this.path=paper.path("M"+entercoords[0]+","+entercoords[1]+"L"+otherend[0]+","
-    +otherend[1]).attr({stroke:"none"});
+  this.path=paper.path("M"+entercoords[0]+","+entercoords[1]+"L"+otherend[0]+","+otherend[1]);
   this.path.node.setAttribute("class","pathState0");
   this.plane=paper.image(planeImages[this.type],entercoords[0],entercoords[1],40,40);
   this.plane.node.setAttribute("parentPlaneId",this.id);
   this.plane.node.onmousedown=function(e) {
-    var _this=planes[Number(this.attributes["parentPlaneId"].nodeValue)];
+    var _this=planes[Number(this.attributes.parentPlaneId.nodeValue)];
+    console.log("Plane with id "+_this.id+" mouse down");
     _this.path.node.setAttribute("class","pathState1");
     currentPlaneSelected=_this.id;
     window.onmousemove=function(e){ //no work... probably
@@ -74,27 +74,27 @@ function Plane(type,entrance,emergency) {
        */
       var _this=planes[currentPlaneSelected];
       var d=_this.path.node.attributes.d.nodeValue;
-      d=d.substring(d.lastIndexOf("L")).split(",",2); //array = x and y
+      d=d.substring(d.lastIndexOf("L")+1).split(",",2); //array = x and y
       d[0]=Number(d[0]);d[1]=Number(d[1]); //convert to numbers
       //Pythagorean
       var l=Math.sqrt(Math.pow(pos.x-d[0],2)+Math.pow(pos.y-d[1],2));
       if (l>5) { //more than 5 pixels change
+        console.log("Plane with id "+_this.id+" captured good mouse move to "+pos.x+","+pos.y+" from "+d[0]+","+d[1]+" with length "+l);
         _this.length+=l;
         _this.path.node.attributes.d.nodeValue+="L"+pos.x+","+pos.y;
       }
     };
-  };
-  this.plane.node.onmouseup=function(e) {
-    if (!e) { e=window.event; } pos=getMouseCoords(e);
-    if (window.onmousemove) { 
-    /* in some cases like we drag it over a runway it automatically ends even though the mouse is still down, in which case we will remove window.onmousemove to stop wasting energy and to tell us here */
-      window.onmousemove=undefined;
-      // we are going to put a little dot at the end
-      var _this=planes[currentPlaneSelected];
-      _this.marker=paper.ellipse(pos.x,pos.y,2,2).attr({fill: "#f00"});
-      _this.path.node.setAttribute("class","pathState2");
-      window.currentPlaneSelected=null;
-    }
+    window.onmouseup=function(e) {
+      console.log("Plane with id "+_this.id+" mouse up");
+      if (window.onmousemove) {
+      /* in some cases like we drag it over a runway it automatically ends even though the mouse is still down, in which case we will remove window.onmousemove to stop wasting energy and to tell us here */
+        window.onmousemove=undefined;
+        // we are going to put a little dot at the end
+        _this.marker=paper.ellipse(pos.x,pos.y,2,2).attr({fill: "#f00"});
+        _this.path.node.setAttribute("class","pathState2");
+        window.currentPlaneSelected=null;
+      }
+    };
   };
   this.length=Math.sqrt(Math.pow(entercoords[0]-otherend[0],2)
     +Math.pow(entercoords[1]-otherend[1],2));
@@ -103,6 +103,8 @@ function Plane(type,entrance,emergency) {
     //bounce off the edge of screen
     
   });
+  console.log("Created new plane with id "+this.id+": ");
+  return this;
 }
 Plane.prototype.remove=function() {
   this.plane.animate({opacity:0},1000,function() {
